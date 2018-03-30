d5d6a908cccc80cb21fc9494ddac9439ae1e494d
Andy Woodruff <awoodruff@gmail.com>
2018-03-30 16:57:36 -0400

    geojson, png, and svg downloads

--- contours.js
+++ contours.js
@@ -2,6 +2,7 @@
 var contourCanvas = document.createElement('canvas');
 contourCanvas.id='contours';
 var contourContext;
+var buffer = 5;
 
 // invisible canvas to which Mapzen elevation tiles will be drawn so we can calculate stuff
 var demCanvas = document.createElement('canvas');
@@ -14,14 +15,15 @@ var demContext = demCanvas.getContext('2d');
 
 // not too big or this can get hella slow
 var mapNode = d3.select('#map').node();
-var width = mapNode.offsetWidth;
-var height = mapNode.offsetHeight;
+var width = mapNode.offsetWidth + 2*buffer;
+var height = mapNode.offsetHeight + 2*buffer;
 contourCanvas.width = width;
 contourCanvas.height = height;
 demCanvas.width = width;
 demCanvas.height = height;
 
 var path = d3.geoPath().context(contourContext);
+var svgPath = d3.geoPath();
 
 var min;
 var max;
@@ -52,9 +54,11 @@ var hypsoColor = d3.scaleLinear()
   .range(["#486341", "#e5d9c9"])
   .interpolate(d3.interpolateHcl);
 
+var contourSVG;
+
 window.onresize = function () {
-  width = mapNode.offsetWidth;
-  height = mapNode.offsetHeight;
+  width = mapNode.offsetWidth + 2*buffer;
+  height = mapNode.offsetHeight + 2*buffer;
   contourCanvas.width = width;
   contourCanvas.height = height;
   demCanvas.width = width;
@@ -175,6 +179,10 @@ d3.select('#hypso-high-color').on('change', function () {
   wait = setTimeout(drawContours,500);
 });
 
+d3.selectAll('#download-geojson, .settings-row.geojson .settings-title').on('click', downloadGeoJson);
+d3.selectAll('#download-png, .settings-row.png .settings-title').on('click', downloadPNG);
+d3.selectAll('#download-svg, .settings-row.svg .settings-title').on('click', downloadSVG);
+
 var exampleLocations = [
   {name: 'Mount Fuji', coords: [35.3577, 138.7331, 13]},
   {name: 'Big Island, Hawaii', coords: [19.6801, -155.5132, 9]},
@@ -237,10 +245,11 @@ pane.appendChild(contourCanvas);
 var labelPane = map.createPane('labels');
 var referenceLayer = L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner-hybrid/{z}/{x}/{y}.png', {pane:'labels', attribution:'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'}).addTo(map);
 
+reverseTransform();
 
 // this resets our canvas back to top left of the window after panning the map
 function reverseTransform() {
-  var top_left = map.containerPointToLayerPoint([0, 0]);
+  var top_left = map.containerPointToLayerPoint([-buffer, -buffer]);
   L.DomUtil.setPosition(contourCanvas, top_left);
 };
 
@@ -252,7 +261,7 @@ function getRelief(){
   // reset DEM data by drawing elevation tiles to it
   for (var t in demLayer._tiles) {
     var rect = demLayer._tiles[t].el.getBoundingClientRect();
-    demContext.drawImage(demLayer._tiles[t].el.img,rect.left,rect.top);
+    demContext.drawImage(demLayer._tiles[t].el.img,rect.left + buffer,rect.top + buffer);
   }
   demImageData = demContext.getImageData(0,0,width,height);
   demData = demImageData.data;
@@ -308,72 +317,104 @@ function getContours () {
   drawContours();
 }
 
-function drawContours() {
-  contourContext.clearRect(0,0,width,height);
-  contourContext.save();
-  if (type == 'illuminated') {
-    contourContext.lineWidth = shadowSize + 1;
-    contourContext.shadowBlur = shadowSize;
-    contourContext.shadowOffsetX = shadowSize;
-    contourContext.shadowOffsetY = shadowSize;
+function drawContours(svg) {
+  if (!svg) {
+    contourContext.clearRect(0,0,width,height);
+    contourContext.save();
+    if (type == 'illuminated') {
+      contourContext.lineWidth = shadowSize + 1;
+      contourContext.shadowBlur = shadowSize;
+      contourContext.shadowOffsetX = shadowSize;
+      contourContext.shadowOffsetY = shadowSize;
 
-    contoursGeoData.forEach(function (c) {
-      contourContext.beginPath();
-      if (c.value < 0) {
-        // blue-ish shadow and highlight colors below sea level
-        contourContext.shadowColor = '#4e5c66';
-        contourContext.strokeStyle = 'rgba(224, 242, 255, .25)';
-      } else {
-        contourContext.shadowColor = shadowColor;
-        contourContext.strokeStyle = highlightColor;
-      }
-      if (colorType == 'hypso')
-        contourContext.fillStyle = hypsoColor(c.value);
-      else if (colorType == 'solid') contourContext.fillStyle = solidColor;
-      else contourContext.fillStyle = '#fff';
-      path(c);
-      // draw the light stroke first, then the fill with drop shadow
-      // the effect is a light edge on side and dark on the other, giving the raised/illuminated contour appearance
-      contourContext.stroke(); 
-      contourContext.fill();
-    });
-  } else {
-    contourContext.lineWidth = lineWidth;
-    contourContext.strokeStyle = lineColor;
-    if (colorType != 'hypso') {
-      contourContext.beginPath();
-      contoursGeoData.forEach(function (c) {
-        if (majorInterval == 0 || c.value % majorInterval != 0) path(c);
-      });
-      if (colorType == 'solid') {
-        contourContext.fillStyle = solidColor;
-        contourContext.fill();
-      }
-      contourContext.stroke();
-    } else {
       contoursGeoData.forEach(function (c) {
         contourContext.beginPath();
-        if (majorInterval == 0 || c.value % majorInterval != 0) path(c);
-        contourContext.fillStyle = hypsoColor(c.value);
+        if (c.value < 0) {
+          // blue-ish shadow and highlight colors below sea level
+          contourContext.shadowColor = '#4e5c66';
+          contourContext.strokeStyle = 'rgba(224, 242, 255, .25)';
+        } else {
+          contourContext.shadowColor = shadowColor;
+          contourContext.strokeStyle = highlightColor;
+        }
+        if (colorType == 'hypso')
+          contourContext.fillStyle = hypsoColor(c.value);
+        else if (colorType == 'solid') contourContext.fillStyle = solidColor;
+        else contourContext.fillStyle = '#fff';
+        path(c);
+        // draw the light stroke first, then the fill with drop shadow
+        // the effect is a light edge on side and dark on the other, giving the raised/illuminated contour appearance
+        contourContext.stroke(); 
         contourContext.fill();
+      });
+    } else {
+      contourContext.lineWidth = lineWidth;
+      contourContext.strokeStyle = lineColor;
+      if (colorType != 'hypso') {
+        contourContext.beginPath();
+        contoursGeoData.forEach(function (c) {
+          if (majorInterval == 0 || c.value % majorInterval != 0) path(c);
+        });
+        if (colorType == 'solid') {
+          contourContext.fillStyle = solidColor;
+          contourContext.fill();
+        }
         contourContext.stroke();
-      });
-    }
-    
-    if (majorInterval != 0) {
-      contourContext.lineWidth = lineWidthMajor;
-      contourContext.beginPath();
-      contoursGeoData.forEach(function (c) {
-        if (c.value % majorInterval == 0) path(c);
-      });
-      contourContext.stroke();
-    }
+      } else {
+        contoursGeoData.forEach(function (c) {
+          contourContext.beginPath();
+          if (majorInterval == 0 || c.value % majorInterval != 0) path(c);
+          contourContext.fillStyle = hypsoColor(c.value);
+          contourContext.fill();
+          contourContext.stroke();
+        });
+      }
+      
+      if (majorInterval != 0) {
+        contourContext.lineWidth = lineWidthMajor;
+        contourContext.beginPath();
+        contoursGeoData.forEach(function (c) {
+          if (c.value % majorInterval == 0) path(c);
+        });
+        contourContext.stroke();
+      }
 
+    }
+    contourContext.restore();
+  } else {
+    if (!contourSVG) {
+      contourSVG = d3.select('body').append('svg');
+    }
+    contourSVG
+      .attr('width', width)
+      .attr('height', height)
+      .selectAll('path').remove();
+
+    contourSVG.selectAll('path.stroke')
+      .data(contoursGeoData)
+      .enter()
+      .append('path')
+      .attr('d', svgPath)
+      .attr('stroke', type == 'lines' ? lineColor : highlightColor)
+      .attr('stroke-width', function (d) {
+        return type == 'lines' ? (majorInterval != 0 && d.value % majorInterval == 0 ? lineWidthMajor : lineWidth) : shadowSize;
+      })
+      .attr('fill', function (d) {
+        if (colorType == 'solid') {
+          return solidColor;
+        } else if (colorType == 'hypso') {
+          return hypsoColor(d.value);
+        } else {
+          return 'none';
+        }
+      })
+      .attr('id', function (d) {
+        return 'elev-' + d.value;
+      });
   }
-  contourContext.restore();
 }
 
-function toGeoJson () {
+function downloadGeoJson () {
   var geojson = {type: 'FeatureCollection', features: []};
   contoursGeoData.forEach(function (c) {
     var feature = {type:'Feature', properties:{elevation: c.value}, geometry: {type:c.type, coordinates:[]}};
@@ -391,7 +432,71 @@ function toGeoJson () {
       });
     })
   });
-  console.log(JSON.stringify(geojson));
+  download(JSON.stringify(geojson), 'countours.geojson');
+}
+
+function downloadPNG () {
+  var newCanvas = document.createElement('canvas');
+  newCanvas.width = width - 2*buffer;
+  newCanvas.height = height - 2*buffer;
+  newCanvas.getContext('2d').putImageData(contourContext.getImageData(0,0,width,height), -buffer, -buffer)
+  // https://stackoverflow.com/questions/12796513/html5-canvas-to-png-file
+  var dt = newCanvas.toDataURL('image/png');
+  /* Change MIME type to trick the browser to downlaod the file instead of displaying it */
+  dt = dt.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
+
+  /* In addition to <a>'s "download" attribute, you can define HTTP-style headers */
+  dt = dt.replace(/^data:application\/octet-stream/, 'data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=Canvas.png');
+
+  var tempLink = document.createElement('a');
+  tempLink.style.display = 'none';
+  tempLink.href = dt;
+  tempLink.setAttribute('download', 'contours.png');
+  if (typeof tempLink.download === 'undefined') {
+      tempLink.setAttribute('target', '_blank');
+  }
+  
+  document.body.appendChild(tempLink);
+  tempLink.click();
+  document.body.removeChild(tempLink);
+}
+
+function downloadSVG () {
+  drawContours(true);
+  var svgData = contourSVG.node().outerHTML;
+  download(svgData, 'contours.svg', 'image/svg+xml;charset=utf-8')
+}
+
+// https://github.com/kennethjiang/js-file-download
+function download(data, filename, mime) {
+    var blob = new Blob([data], {type: mime || 'application/octet-stream'});
+    if (typeof window.navigator.msSaveBlob !== 'undefined') {
+        // IE workaround for "HTML7007: One or more blob URLs were 
+        // revoked by closing the blob for which they were created. 
+        // These URLs will no longer resolve as the data backing 
+        // the URL has been freed."
+        window.navigator.msSaveBlob(blob, filename);
+    }
+    else {
+        var blobURL = window.URL.createObjectURL(blob);
+        var tempLink = document.createElement('a');
+        tempLink.style.display = 'none';
+        tempLink.href = blobURL;
+        tempLink.setAttribute('download', filename); 
+        
+        // Safari thinks _blank anchor are pop ups. We only want to set _blank
+        // target if the browser does not support the HTML5 download attribute.
+        // This allows you to download files in desktop safari if pop up blocking 
+        // is enabled.
+        if (typeof tempLink.download === 'undefined') {
+            tempLink.setAttribute('target', '_blank');
+        }
+        
+        document.body.appendChild(tempLink);
+        tempLink.click();
+        document.body.removeChild(tempLink);
+        window.URL.revokeObjectURL(blobURL);
+    }
 }
 
 // convert elevation tile color to elevation value
